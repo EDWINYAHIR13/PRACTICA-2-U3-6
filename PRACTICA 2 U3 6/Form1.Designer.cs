43c5ae293e23c3d9790f8abba30aa9259140abea
tiago <tiago@DESKTOP-00HI8IK>
2023-11-03 21:21:06 -0500

    Agregar archivos de proyecto.

---
+++ PRACTICA 2 U3 6/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PRACTICA_2_U3_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(145, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(514, 33);
+            label1.TabIndex = 0;
+            label1.Text = "Verificar si un número es par ó impar";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(83, 140);
+            label2.Name = "label2";
+            label2.Size = new Size(189, 24);
+            label2.TabIndex = 1;
+            label2.Text = "Escribe un número";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(291, 137);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 30);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Honeydew;
+            button1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(291, 233);
+            button1.Name = "button1";
+            button1.Size = new Size(131, 44);
+            button1.TabIndex = 3;
+            button1.Text = "Verifica";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 24;
+            listBox1.Location = new Point(558, 113);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(173, 292);
+            listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGreen;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private ListBox listBox1;
+    }
+}
